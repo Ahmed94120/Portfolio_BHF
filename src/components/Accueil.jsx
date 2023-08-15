a78b5cde0cc5dbaed946ef9d22215df020584348
Edra3k <ahmedbouhafa101@gmail.com>
2023-08-15 16:38:06 +0200

    correction name profile.jpeg

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profile from "../assets/profile.jpeg";
+import profile from "../assets/Profile.jpeg";
 import { RxDoubleArrowRight } from "react-icons/rx";
 
 const Accueil = () => {
