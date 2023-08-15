6803a53b2cfefdae32f03e100273c4552e8f02fb
Edra3k <ahmedbouhafa101@gmail.com>
2023-08-15 14:17:57 +0200

    Page accueil finit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import NavBar from "./components/NavBar";
 import Accueil from "./components/Accueil";
 
-
-
-
 function App() {
-
-  return (
-    <div>
-      <NavBar /> 
-      <Accueil />
-      
-    </div>
-  );
+   return (
+      <div>
+         <NavBar />
+         <Accueil />
+      </div>
+   );
 }
 
-export default App
+export default App;
