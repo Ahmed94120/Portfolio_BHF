fbe1e5e3d78098dbe01947ffa76e37431dd5c8c7
Edra3k <ahmedbouhafa101@gmail.com>
2023-08-15 11:48:11 +0200

    Portfolio_main

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from "./components/NavBar";
+import Accueil from "./components/Accueil";
+
+
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBar /> 
+      <Accueil />
+      
+    </div>
+  );
+}
+
+export default App
---
+++ src/components/Accueil.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import reactS from '../assets/react.svg'
+
+const Accueil = () => {
+  return (
+ 
+    <div>
+      <h1 className='text-6xl'>Accueil</h1>
+    </div>
+  )
+}
+
+export default Accueil
