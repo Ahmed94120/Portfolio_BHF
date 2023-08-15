2d67c6bc1d5a9366ddc8e073d31cc03f68d0af82
Edra3k <ahmedbouhafa101@gmail.com>
2023-08-15 14:16:48 +0200

    page accueil finit

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import reactS from '../assets/react.svg'
+import React from "react";
+import profile from "../assets/profile.jpeg";
+import { RxDoubleArrowRight } from "react-icons/rx";
 
 const Accueil = () => {
-  return (
- 
-    <div>
-      <h1 className='text-6xl'>Accueil</h1>
-    </div>
-  )
-}
+   return (
+      <div
+         name="home"
+         className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 "
+      >
+         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4  md:flex-row">
+            <div className="flex flex-col justify-center h-full mf:mt-100">
+               <h2 className="text-3xl sm:text-6xl front-bold text-white">
+                  🚀 Bienvenue sur mon site de prestations! 🌐
+               </h2>
+               <p className="text-gray-500 py-4 max-w-md">
+                  {" "}
+                  Salut, je suis Ahmed, passionné par la technologie et la
+                  créativité. Je suis développeur web freelance et étudiant en
+                  ingénierie de la cybersécurité. Avec des compétences en React,
+                  Vue.js, Laravel et WordPress, je crée des sites web modernes
+                  et uniques qui répondent à vos besoins. Explorez mes services
+                  sur mon site et contactez-moi via la page de contact.
+               </p>
 
-export default Accueil
+               <div>
+                  <button className=" group text-white w-fit px-7 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                     Portfolio &nbsp;
+                     <span className="group-hover:rotate-90 duration-300">
+                        <RxDoubleArrowRight size={20} className="ml-1" />
+                     </span>
+                  </button>
+               </div>
+            </div>
+            <div>
+               <img
+                  src={profile}
+                  alt="my profile"
+                  className="rounded-2xl mx-auto w-2/3 md:w-full "
+               />
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default Accueil;
